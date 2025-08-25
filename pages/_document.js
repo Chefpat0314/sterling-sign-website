f64c1e37e9bf8cb4e31d64348e185d93c94edffa
Patrick Sterling Kimball <patrick@sterlingsignsolutions.com>
2025-08-24 21:03:14 -0700

    Add GA4 snippet reading NEXT_PUBLIC_GA4_ID

--- pages/_document.js
+++ pages/_document.js
@@ -2,16 +2,31 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const gaId = process.env.NEXT_PUBLIC_GA4_ID;
+
   return (
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-
-        {/* Preload the actual logo you use */}
         <link rel="preload" as="image" href="/images/logo.png" />
-        {/* Remove or keep this only if you actually use a hero video */}
-        {/* <link rel="preload" as="video" href="/videos/hero.mp4" /> */}
+
+        {/* Google Analytics 4 */}
+        {gaId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${gaId}');
+                `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <body className="bg-white text-slate-900">
         <Main />
